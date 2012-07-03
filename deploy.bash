1453390031f77d3b3a9883f2a70a1669feeb00d2
MaskRay <emacsray@gmail.com>
2012-07-03 22:34:11 +0800

    +deploy.bash

---
+++ deploy.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+files=$(git ls-files)
+target=~
+
+for f in $files; do
+  if [[ "$f" =~ ^home/ ]]; then
+    :
+  else
+    continue
+  fi
+
+  echo copying $f
+  g="$target/${f/home\//}"
+  mkdir -p "${g%/*}"
+  if ! [[ -L "$g" ]]; then
+    if [[ ! -f "$g" || "$f" -nt "$g" ]]; then
+      cp "$f" "$g"
+    else
+      if ! diff -q "$f" "$g"; then
+	vimdiff "$f" "$g"
+      fi
+    fi
+  fi
+done
