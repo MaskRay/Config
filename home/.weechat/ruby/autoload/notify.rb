5035495e7b620dedc3889880464d4b1e09457ca4
MaskRay <emacsray@gmail.com>
2012-07-08 22:51:17 +0800

    * .weechat/ruby/autoload/notify.rb switch to hook_signal weechat_pv weechat_highlight

--- home/.weechat/ruby/autoload/notify.rb
+++ home/.weechat/ruby/autoload/notify.rb
@@ -1,18 +1,16 @@
 require 'ruby-growl'
 
 def weechat_init
-  Weechat.register('notify', 'Ray Song', '0.0', 'GPL', 'notify', '', '')
-  Weechat.hook_print '', 'irc_privmsg', '', 1, 'notify', ''
+  Weechat.register 'notify', 'Ray Song', '0.0', 'GPL', 'notify', '', ''
+  Weechat.hook_signal "weechat_pv", "notify", "" 
+  Weechat.hook_signal "weechat_highlight", "notify", "" 
   Weechat::WEECHAT_RC_OK
 end
 
-def notify data, buffer, date, tags, displayed, highlight, prefix, message
+def notify data, signal, message
   g = Growl.new 'localhost', 'ruby-growl'
   g.add_notification 'weechat'
-
-  if Weechat.buffer_get_string(buffer, 'localvar_type') == 'private' or highlight == '1'
-    g.notify 'weechat', prefix, message
-  end
-
+  channel, message = message.split "\t", 2
+  g.notify 'weechat', channel, message
   Weechat::WEECHAT_RC_OK
 end
