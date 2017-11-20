718646b48aca13bff4098f9025a60c500117c168
Fangrui Song <i@maskray.me>
2017-11-19 19:58:25 -0800

    emacs

--- home/.emacs.d/layers/+my/my-cquery/packages.el
+++ home/.emacs.d/layers/+my/my-cquery/packages.el
@@ -1,15 +1,12 @@
-(defconst my-cquery-packages '(cquery))
+(defconst my-cquery-packages '(lsp-mode))
 
-(defun my-cquery/init-cquery ()
+(defun my-cquery/post-init-lsp-mode ()
   (add-to-load-path (expand-file-name "~/.emacs.d/private/local"))
   (use-package lsp-cquery
     :after lsp-mode
     :config
     (setq cquery/root-dir (expand-file-name "~/Dev/Util/cquery/"))
     (add-hook 'c-mode-common-hook #'my//enable-cquery-if-compile-commands-json))
-  )
-
-(defun my-cquery/post-init-cquery ()
   ;; (spacemacs/set-leader-keys
   ;;   "jl" (lambda ()
   ;;          (interactive)
