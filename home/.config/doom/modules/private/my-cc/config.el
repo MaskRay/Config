0e22c4a34ddcb92123814404c1c6838560f259b6
Fangrui Song <i@maskray.me>
2019-12-05 16:43:52 -0800

    git

--- home/.config/doom/modules/private/my-cc/config.el
+++ home/.config/doom/modules/private/my-cc/config.el
@@ -85,7 +85,7 @@
       ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
        "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
        "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
-      :extraArgs ["--gcc-toolchain=/usr"]
+      :extraArgs []
       :pathMappings ,+ccls-path-mappings)
      :completion
      (:include
