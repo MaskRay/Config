0ddc20ebd31483e980ac7e60cabd08cedea7843e
Fangrui Song <i@maskray.me>
2021-10-25 19:33:46 -0700

    init.lua

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -7,20 +7,18 @@ treesitter.setup {
       enable = true,
       set_jumps = true,
       goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
+        [']m'] = '@class.outer',
+        [']]'] = '@function.outer',
       },
       goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
+        [']M'] = '@class.outer',
       },
       goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
+        ['[m'] = '@class.outer',
+        ['[['] = '@function.outer',
       },
       goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
+        ['[M'] = '@class.outer',
       },
     },
     select = {
