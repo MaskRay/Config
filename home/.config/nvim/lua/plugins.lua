48435f8797bf28155c70da22b81f4ea44d7ec821
Fangrui Song <i@maskray.me>
2025-03-19 23:06:10 -0700

    telescope

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -63,31 +63,23 @@ require('telescope').setup{
       i = {
         ['<C-h>'] = 'which_key',
         ['<M-n>'] = require('telescope.actions').insert_original_cword,
+        ['<C-w>'] = function(prompt_bufnr)
+          -- find parent directory https://github.com/nvim-telescope/telescope.nvim/issues/2179
+          local current_picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+          local cwd = current_picker.cwd and tostring(current_picker.cwd)
+              or vim.loop.cwd()
+          local parent_dir = vim.fs.dirname(cwd)
+
+          require('telescope.actions').close(prompt_bufnr)
+          require('telescope.builtin').find_files {
+            prompt_title = vim.fs.basename(parent_dir),
+            cwd = parent_dir,
+          }
+        end,
       }
     }
   },
   extensions = {fzf = {}},
-  pickers = {
-    find_files = {
-      mappings = {
-        i = {
-          ['<C-w>'] = function(prompt_bufnr)
-            -- find parent directory https://github.com/nvim-telescope/telescope.nvim/issues/2179
-            local current_picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
-            local cwd = current_picker.cwd and tostring(current_picker.cwd)
-            or vim.loop.cwd()
-            local parent_dir = vim.fs.dirname(cwd)
-
-            require('telescope.actions').close(prompt_bufnr)
-            require('telescope.builtin').find_files {
-              prompt_title = vim.fs.basename(parent_dir),
-              cwd = parent_dir,
-            }
-          end,
-        },
-      },
-    },
-  },
 }
 require'telescope'.load_extension'fzf'
 
