7b0f96e00e226aaf40e6d95626e2a5d4b508b1b5
Fangrui Song <i@maskray.me>
2025-06-22 17:33:40 -0700

    neovim for server

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -258,7 +258,7 @@ for _, lsp in ipairs(servers) do
     options = vim.tbl_extend('force', options, {
       init_options = {
         index = {
-          threads = 0,
+          threads = 50,
           initialBlacklist = {"/(test|unittests)/"},
         };
         highlight = {
